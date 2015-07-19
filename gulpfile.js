52c791f46a7fe2fee0c8814f82a6741b87f0842f
Sylvain Reucherand <sylvain.reucherand@gmail.com>
2015-07-19 16:52:40 +0200

    Merge gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -11,13 +11,8 @@ gulp.task('deploy', shell.task([
     'git checkout -b ' + branch,
     'npm run build',
     'git add static/',
-<<<<<<< Updated upstream
     'git commit -am "' + branch + '"',
-    'git push heroku ' + branch + ':master --force',$
-=======
-    'git commit -am "' + new Date().getTime() + '"',
-    'git push heroku ' + new Date().getTime() + ':master --force',
->>>>>>> Stashed changes
+    'git push heroku ' + branch + ':master --force',
     'git checkout master',
     'git branch -D ' + branch,
     'git stash pop'
