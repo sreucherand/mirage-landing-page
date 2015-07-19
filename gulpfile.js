329b508230f3390dddb698185fb302a3f3d33fc6
Sylvain Reucherand <sylvain.reucherand@gmail.com>
2015-07-19 16:46:49 +0200

    Fix gulpfile branch mistakes

--- gulpfile.js
+++ gulpfile.js
@@ -11,8 +11,8 @@ gulp.task('deploy', shell.task([
     'git checkout -b ' + branch,
     'npm run build',
     'git add /static',
-    'git commit -am "' + new Date().getTime() + '"',
-    'git push heroku ' + new Date().getTime() + ':master --force',
+    'git commit -am "' + branch + '"',
+    'git push heroku ' + branch + ':master --force',
     'git checkout master',
     'git branch -D ' + branch,
     'git stash pop'
